c2446b9e123a2f593616a9a4850ae16ea5a9a79b
ibevers <isaacbevers@gmail.com>
2024-05-20 15:57:55 -0400

    Remove potentially unnecessary lines in files and rebuild deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const config = require('./src/config');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/demo-protocol/' : '/',
   chainWebpack: config => {
     config.module
       .rule("vue")
